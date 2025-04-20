570e1e292b56a3c8baaabd2c5d5124c1397fada5
Charles <charley43613@gmail.com>
2025-04-19 21:07:22 -0400

    misconfigured url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  rootUrl: `https://pokemongo-be-master.onrender.com/v1/deck/me`,
+  rootUrl: `https://pokemongo-be-master.onrender.com/api/v1/deck/me`,
   serviceEndpoint: 'https://pokemongo-be-master.onrender.com/api/v1'
 };
  
