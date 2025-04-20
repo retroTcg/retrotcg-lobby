9cdb479788a264d500551837e1e8923a872b10eb
Charles <charley43613@gmail.com>
2025-04-19 20:55:35 -0400

    endpoint to point to backend vs whatever the hell i was doing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  rootUrl: `https://retrotcg.netlify.app/api/v1/deck/me`,
-  serviceEndpoint: 'https://retrotcg.netlify.app/api/v1'
+  rootUrl: `https://pokemongo-be-master.onrender.com/v1/deck/me`,
+  serviceEndpoint: 'https://pokemongo-be-master.onrender.com/api/v1'
 };
  
